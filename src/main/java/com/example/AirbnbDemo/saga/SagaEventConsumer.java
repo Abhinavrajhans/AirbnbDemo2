f929e1d7ef40728b24d1d9713771fe442152b1fa
abhinavrajhans <abhinav.rajhans1@gmail.com>
2026-02-24 18:25:29 +0530

    Completed The SagaEventConsumer Code

--- src/main/java/com/example/AirbnbDemo/saga/SagaEventConsumer.java
+++ src/main/java/com/example/AirbnbDemo/saga/SagaEventConsumer.java
@@ -1,23 +1,37 @@
 package com.example.AirbnbDemo.saga;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import tools.jackson.databind.ObjectMapper;
+import java.util.concurrent.TimeUnit;
 
 @Component
 @RequiredArgsConstructor
-
+@Slf4j
 public class SagaEventConsumer {
 
     private static final String SAGA_QUEUE = "saga:events";
     private final RedisTemplate<String,String> redisTemplate;
     private final ObjectMapper objectMapper;
+    private final SagaEventProcessor sagaEventProcessor;
 
     @Scheduled(fixedDelay = 500) //poll every 500 mili seconds
     public void consumeEvents(){
-
+        try{
+            String event=redisTemplate.opsForList().leftPop(SAGA_QUEUE,1, TimeUnit.SECONDS);
+            if(event!=null && !event.isEmpty()){
+                SagaEvent sagaEvent = objectMapper.readValue(event, SagaEvent.class);
+                log.info("Processing SagaEvent {}", sagaEvent.toString());
+                sagaEventProcessor.processEvent(sagaEvent);
+                log.info("SagaEvent Processed Successfully for saga Id:{}", sagaEvent.toString());
+            }
+        }
+        catch (Exception e){
+            log.error("Error Processing saga events: {}", e.getMessage());
+            throw new RuntimeException("Failed to process Saga Event",e);
+        }
     }
-
 }
